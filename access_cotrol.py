f69d63bd91bcc5fb2be89ff23ab1185599bc1723
ElvisOmbiro <elvisombiro99@gmail.com>
2024-06-12 14:50:26 +0300

    removing access control

--- access_cotrol.py
+++
@@ -1,7 +0,0 @@
-@app.route('/todos', methods=['GET', 'POST'])
-def manage_todos():
-    if 'username' in session:
-        username = session['username']
-        # Fetch or save to-do items specific to the logged-in user
-        return 'User-specific to-do list'
-
